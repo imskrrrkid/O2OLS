e1744f4b1aa90fff2303f51c6ca139c4b279f7b9
imskrrrkid <imskrrrkid@gmail.com>
2026-01-26 01:39:31 +0500

    Blockout Ui 30%

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,41 @@ import { MapContainer, TileLayer } from "react-leaflet";
 function App() {
   return (
     <>
-      <div className="title">
-        <h1>
-          One Two One Location Share
-          {<br />} <a href="https://github.com/imskrrrkid/O2OLS">GitHub</a> for
-          more Details
-        </h1>
+      <div class="Header">
+        <ul>
+          <li>
+            <a>File</a>
+          </li>
+          <li>
+            <a>Edit</a>
+          </li>
+          <li>
+            <a>More</a>
+          </li>
+          <li>
+            <a>Account</a>
+          </li>
+        </ul>
       </div>
-      <div className="map">
-        <MapContainer className="mapCont" center={[30.3753, 69.3451]} zoom={3}>
-          <TileLayer
-            attribution='"&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors"'
-            url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-          />
-        </MapContainer>
+      <div class="container">
+        <div class="cont-dock"></div>
+        <div class="cont-mapxdetails">
+          <div class="mapcont">
+            <div className="map">
+              <MapContainer
+                className="mapCont"
+                center={[30.3753, 69.3451]}
+                zoom={3}
+              >
+                <TileLayer
+                  attribution='"&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors"'
+                  url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+                />
+              </MapContainer>
+            </div>
+          </div>
+          <div class="details">asdsad33</div>
+        </div>
       </div>
     </>
   );
